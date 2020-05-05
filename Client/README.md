a163201de4fe899fb668e8ec3be0670a3507bd07
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-05-05 14:23:33 +0200

    restructure client directory

---
+++ Client/README.md
@@ -0,0 +1,6 @@
+    cd vdcs-preprocessor
+    make
+    ./v2go <output-file> < <input-file>
+
+    go run <output-file> client_username DS_public_key DS_username
+    wallet_password
--- Client/test.go
+++ Client/test.go
@@ -27,19 +27,24 @@ func main() {
 
 	//VDCS Communications:
 	_myEqual_string_1_string_1Ch0 := make(chan vdcs.ChannelContainer)
-   	go vdcs.Comm("myEqual_string_1_string_1", 0, 4, 1, _myEqual_string_1_string_1Ch0)
+   	go vdcs.Comm("myEqual_string_1_string_1", 0, 6, 1, _myEqual_string_1_string_1Ch0)
 	_myEqual_string_1_string_1Ch1 := make(chan vdcs.ChannelContainer)
-   	go vdcs.Comm("myEqual_string_1_string_1", 1, 4, 1, _myEqual_string_1_string_1Ch1)
+   	go vdcs.Comm("myEqual_string_1_string_1", 1, 6, 1, _myEqual_string_1_string_1Ch1)
+	_myEqual_string_1_string_1Ch2 := make(chan vdcs.ChannelContainer)
+   	go vdcs.Comm("myEqual_string_1_string_1", 2, 6, 1, _myEqual_string_1_string_1Ch2)
 
 	//USER PROGRAM:
-	i := "a"
-	j := "b"
-	k := "a"
+	i := "abcdefghij"
+	j := "aaaaaaaaaa"
+	k := "aaaaaaaaaa"
 	result := eval(i, j, 0, _myEqual_string_1_string_1Ch0)
 	fmt.Println("Result_i==j?:")
 	fmt.Println(result)
 	result  = eval(i, k, 1, _myEqual_string_1_string_1Ch1)
 	fmt.Println("Result_i==k?:")
+	fmt.Println(result)
+	result  = eval(j, k, 2, _myEqual_string_1_string_1Ch2)
+	fmt.Println("Result_j==k?:")
 	fmt.Println(result)
 
 }
